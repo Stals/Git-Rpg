8a4acfd251716075a4f741ec6d3f3fc5bc166dc9
Stals <stalsfromhell@gmail.com>
2011-04-11 14:59:22 +0400

    parse-raw builds and changes

--- git/git/git.cpp
+++ git/git/git.cpp
@@ -6,16 +6,41 @@
 //
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 #include <Windows.h>
+#include <map>
+#include <fstream>
+struct lines {
+	int plus;
+	int minus;
+}l;
+std::map<std::string,lines> data;
+int numberOfProjects=0;
+std::string projectDir;
+
+void getData(){
+
+	std::ifstream f("Stals");
+	f>>numberOfProjects;
+
+	for(int i=0;i<numberOfProjects;++i){
+
+		f>>projectDir;
+		f>>l.plus;
+		f>>l.minus;
+
+		data.insert ( std::pair<std::string,lines >(projectDir,l) );
+		
+	}
+
+
+}
 int main()
 {
-	while(true){
-		system("C:/msysgit/msysgit/bin/sh.exe --login -i 'C:/My Dropbox/Programming/BigProjects/C++/GitRPG/main.gs'");
-		
-		//¬ этом месте мы уже получили raw файл, теперь его нужно перегнать в нужный формат
-		
-		Sleep(120000); //each 2 hours
-	}
+	
+	getData();
+
+
 	return 0;
 }
 
